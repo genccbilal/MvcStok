93fbb32def99a493210b754914949c42363c752f
Bilal GENÇ <96884922+genccbilal@users.noreply.github.com>
2025-02-04 23:58:20 +0300

    Add project files.

---
+++ MvcStok/Controllers/MusteriController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+
+        MvcDbStokEntities db =new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLMUSTERILER.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniMusteri()
+		{
+            return View();
+		}
+
+
+        [HttpPost]
+        public ActionResult YeniMusteri(TBLMUSTERILER m1)
+		{
+            db.TBLMUSTERILER.Add(m1);
+            db.SaveChanges();
+            return View();
+		}
+
+
+    }
+}
---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+
+
+        [HttpGet]
+        public ActionResult UrunEkle()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p1)
+		{
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return View();
+		}
+    }
+}
